5550f966bd11665062f969d01667f4d11a9935b5
vrajdoshi <84803078+Vrajdoshi@users.noreply.github.com>
2022-04-05 19:36:52 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,20 @@
-self.addEventListener("push", e => {
-    const data = e.data.json();
-    self.registration.showNotification(
-        data.title, // title of the notification
-        {
-            body: "Push notification from section.io", //the body of the push notification
-            image: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
-            icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/" // icon 
-        }
-    );
+self.addEventListener("push", function(e){
+    var option = {
+        body: "This nottification was generated from a push",
+        icon: "imges/example.png",
+        vibrate: [100, 50, 100],
+        data:  {
+            dataOfArrival: Date.now(),
+            primaryKey:"2"
+        },
+        actions:[
+            {
+                action: 'explore',
+                title:'explore this new world',
+                icon:'images/checkmark.png'
+            },
+            { action: 'close', icon:'images/xmark.png'}
+        ]};
+        e.waitUntil(self.registration.showNotification('hello world!', options) );
+
 });
